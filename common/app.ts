af410dc467884339205ba75c2821d0e9861f7d51
jupl <juan.placencia.512@gmail.com>
2017-04-09 21:13:00 -0500

    Move mobx setup

--- common/app.ts
+++ common/app.ts
@@ -1,13 +1,11 @@
 import {Server} from 'hapi'
 import {IApp} from 'hapiour-decorators'
-import {useStaticRendering} from 'mobx-react'
 
 export default class App implements IApp {
   protected server: Server
 
   constructor(server: Server) {
     this.server = server
-    useStaticRendering(true)
   }
 
   public onStart() {
--- common/stores.ts
+++ common/stores.ts
@@ -1,13 +1,15 @@
 import {useStrict} from 'mobx'
+import {useStaticRendering} from 'mobx-react'
 
 type StoreBuilder<S> = (isServer: boolean) => S
 
 export function buildStoreGetter<S>(createStore: StoreBuilder<S>) {
   let store: S
-  useStrict(true)
   return function getStore(isServer: boolean) {
     if(!store) {
       store = createStore(isServer)
+      useStrict(true)
+      useStaticRendering(isServer)
     }
     return store
   }
